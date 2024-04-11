d2d3cabd5935745f1d02b6b5b46853dd94aebc72
Felipe Cesar <felipecesr@gmail.com>
2024-04-11 19:15:56 -0300

    lesson-07: user event

--- app/__tests__/page.test.js
+++ app/__tests__/page.test.js
@@ -1,14 +1,17 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Page from "../page";
 
-test("shows next question after submit correct answer", () => {
+test("shows next question after submit correct answer", async () => {
+  const user = userEvent.setup();
+
   render(<Page />);
 
   const input = screen.getByLabelText(/what is batman's real name\?/i);
-  input.value = "Bruce Wayne";
+  await user.type(input, "Bruce Wayne");
 
-  const button = document.querySelector("button");
-  fireEvent.click(button);
+  const button = screen.getByRole("button", { name: /submit answer/i });
+  await user.click(button);
 
   expect(screen.getByText(/correct!/i)).toBeInTheDocument();
   expect(
