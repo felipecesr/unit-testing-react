9cc277b28780240f79934441487c335760a9603b
Felipe Cesar <felipecesr@gmail.com>
2024-04-11 19:21:25 -0300

    lesson-08: more tests

--- app/__tests__/page.test.js
+++ app/__tests__/page.test.js
@@ -18,3 +18,37 @@ test("shows next question after submit correct answer", async () => {
     screen.getByText(/what is wonder woman's real name\?/i)
   ).toBeInTheDocument();
 });
+
+test("shows next question after submit wrong answer", async () => {
+  const user = userEvent.setup();
+
+  render(<Page />);
+
+  const input = screen.getByLabelText(/what is batman's real name\?/i);
+  await user.type(input, "Clark Kent");
+
+  const button = screen.getByRole("button", { name: /submit answer/i });
+  await user.click(button);
+
+  expect(
+    screen.getByText(/wrong! the correct answer was bruce wayne/i)
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText(/what is wonder woman's real name\?/i)
+  ).toBeInTheDocument();
+});
+
+test("shows game over text after finish questions", async () => {
+  const user = userEvent.setup();
+
+  render(<Page />);
+
+  const button = screen.getByRole("button", { name: /submit answer/i });
+  await user.click(button);
+  await user.click(button);
+  await user.click(button);
+
+  expect(
+    screen.getByText(/game over, you scored 0 points/i)
+  ).toBeInTheDocument();
+});
